2e4c9154db3a8f2501b6fa274584c537af100c99
Fabio Luiz Vieira da Silva <fabinvieira@gmail.com>
2022-01-05 01:13:03 -0300

    Manipulando DOM com javascript

---
+++ teste.js
@@ -0,0 +1,13 @@
+let lines = gets();
+console.log(lines);
+let N = parseInt(lines.shift());
+console.log(N);
+return;
+//complete o código
+let X = gets();
+
+for (  i=1; i<=X; i++  ) {
+    if(i % 2 !== 0){
+        print(      );
+    }
+}
